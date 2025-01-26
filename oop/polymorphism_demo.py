98d8b592925f832d6d7f1c9d365d7cdb6e9a5c8b
MarouaneOuchala <marouaneouchalla@gmail.com>
2025-01-26 22:38:58 +0100

    Update polymorphism_demo.py

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -20,7 +20,7 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        return math.pi * self.radius**2
+        return math.pi * (self.radius ** 2)
 
 # Testing Polymorphism
 shapes = [
